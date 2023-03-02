a5a5ca2ad907e4762322bb2b9bccbfa239b9705f
Caitlyn-Milne <CaitlynMilne2001@gmail.com>
2023-03-02 07:17:40 +0000

    Added basic quad tree functionality (Add, Contains, Clear)

---
+++ CSharp-Test/MyQuadTreeTests.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+
+namespace CSharp.Tests;
+
+internal class TestPoint : IPoint
+{
+    public bool WasChecked = false;
+    private readonly double _x;
+    private readonly double _y;
+
+    public TestPoint(double x, double y, string data = "")
+    {
+        _x = x;
+        _y = y;
+        Data = data;
+    }
+
+    public readonly string Data;
+
+    public double X
+    {
+        get
+        {
+            WasChecked = true;
+            return _x;
+        }
+    }
+
+    public double Y
+    {
+        get
+        {
+            WasChecked = true;
+            return _y;
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        WasChecked = true;
+        return base.Equals(obj);
+    }
+
+    public static bool operator ==(TestPoint a, TestPoint b)
+    {
+        a!.WasChecked = true;
+        b!.WasChecked = true;
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(TestPoint a, TestPoint b) => !(a == b);
+}
+
+internal class MyQuadTreeTests
+{
+
+    [Test]
+    public void Debug()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        for (var i = 0; i < 100; i++)
+        {
+            var randomString = Guid.NewGuid().ToString();
+            var x = Random.Shared.NextDouble() * 200 - 100;
+            var y = Random.Shared.NextDouble() * 200 - 100;
+            var point = new TestPoint(x, y, randomString);
+            quadTree.Add(point);
+        }
+    }
+
+    [Test]
+    public void WhenConstructing_ShouldBeEmpty()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        Assert.That(quadTree.Length, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void WhenAddingPoint_ShouldContainPoint()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var point = new TestPoint(5, 10, "hi");
+
+        quadTree.Add(point);
+        Assert.True(quadTree.Contains(point));
+    }
+
+    [Test]
+    public void WhenAddingPoint_ShouldIncreaseLength()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var point = new TestPoint(5, 10, "hi");
+
+        quadTree.Add(point);
+        Assert.That(quadTree.Length, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void WhenRemovingPoint_ShouldNotContainPoint()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var point = new TestPoint(5, 10, "hi");
+        quadTree.Add(point);
+
+        quadTree.Remove(point);
+        Assert.False(quadTree.Contains(point));
+    }
+
+    [Test]
+    public void WhenClearing_ShouldBeEmpty()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var point1 = new TestPoint(5, 10, "hi");
+        var point2 = new TestPoint(10, 5, "bye");
+        quadTree.Add(point1);
+        quadTree.Add(point2);
+
+        quadTree.Clear();
+        Assert.That(quadTree.Length, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void WhenFindInRange_ShouldReturnAllInRange()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var x1y1 = new TestPoint(1, 1, "center");
+        var x1y1_99 = new TestPoint(1, 1.99, "just in range");
+        var x2y2 = new TestPoint(2, 2, "just out of range");
+        var x1y10 = new TestPoint(1, 10, "out of range");
+        var x10y1 = new TestPoint(10, 1, "out of range");
+        var x5y5 = new TestPoint(5, 5, "out of range");
+        quadTree.Add(x1y1);
+        quadTree.Add(x1y1_99);
+        quadTree.Add(x2y2);
+        quadTree.Add(x1y10);
+        quadTree.Add(x10y1);
+        quadTree.Add(x5y5);
+
+        var found = quadTree.FindRange(1, 1, 1);
+        Assert.That(found, Is.EquivalentTo(new[] { x1y1 , x1y1_99, x2y2 }));
+    }
+
+    [Test]
+    public void WhenFindingNearest_ShouldReturnNearest()
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var x1y1 = new TestPoint(1, 1, "a");
+        var x2y2 = new TestPoint(2, 2, "b");
+        var x1y10 = new TestPoint(1, 10, "c");
+        var x10y1 = new TestPoint(10, 1, "d");
+        var x5y5 = new TestPoint(5, 5, "e");
+        quadTree.Add(x1y1);
+        quadTree.Add(x2y2);
+        quadTree.Add(x1y10);
+        quadTree.Add(x10y1);
+        quadTree.Add(x5y5);
+
+        var found = quadTree.FindNearest(3, 3);
+        Assert.That(found, Is.EqualTo(x2y2));
+    }
+
+    #region peformance
+    [Test]
+    [TestCase(5,5)]
+    [TestCase(100, 30)]
+    [TestCase(1000, 40)]
+    [TestCase(10000, 50)]
+    [Parallelizable(ParallelScope.Self)]
+    public void WhenFindingNearest_ShouldNotCheckMostPoints(int numPoints,int maxAllowed)
+    {
+        var quadTree = new MyQuadTree<TestPoint>();
+        var points = new List<TestPoint>();
+        for (var i = 0; i < numPoints; i++)
+        {
+            var randomString = Guid.NewGuid().ToString();
+            var x = Random.Shared.NextDouble() * 200 - 100;
+            var y = Random.Shared.NextDouble() * 200 - 100;
+            var point = new TestPoint(x, y, randomString);
+            quadTree.Add(point);
+            points.Add(point);
+        }
+        foreach (var point in points)
+        {
+            point.WasChecked = false;
+        }
+
+        quadTree.FindNearest(5,5);
+        var pointsChecked = points.Count(p => p.WasChecked);
+        Assert.That(pointsChecked, Is.LessThanOrEqualTo(maxAllowed));
+    }
+    #endregion
+
+}
+
---
+++ CSharp/MyQuadTree.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures;
+
+public interface IPoint
+{
+    public double X { get; }
+    public double Y { get; }
+}
+
+public interface IQuadTree<T>
+    where T : IPoint
+{
+    public void Add(T point);
+    public void Remove(T point);
+    public void Clear();
+    public bool Contains(T point);
+    public T? FindNearest(double x, double y);
+
+    public IEnumerable<T> FindRange(double x, double y, double range);
+
+    public int Length { get; }
+
+    public int QuadCapacity { get; }
+}
+
+public class MyQuadTree<T> : IQuadTree<T> where T : IPoint
+{
+    private MyQuad<T> _root;
+
+    public MyQuadTree()
+    {
+        _root = new MyQuad<T>(this, new Boundary(-100, 100, -100, 100)); //todo auto grow
+    }
+
+    public void Add(T point)
+    {
+        Length++;
+        _root.Add(point);
+    }
+
+    public void Remove(T point)
+    {
+        Length--;
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        Length = 0;
+        _root = new MyQuad<T>(this, _root.Bounds);
+    }
+
+    public bool Contains(T point)
+    {
+        return _root.Contains(point);
+    }
+
+    public T? FindNearest(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> FindRange(double x, double y, double range)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Length { get; private set; }
+    public int QuadCapacity => 4;
+}
+
+internal class Boundary
+{
+    public Boundary(double xMin, double xMax, double yMin, double yMax)
+    {
+        XMin = xMin;
+        XMax = xMax;
+        YMin = yMin;
+        YMax = yMax;
+    }
+
+    public double XMin { get; init; }
+    public double XMax { get; init; }
+    public double YMin { get; init; }
+    public double YMax { get; init; }
+
+    public double XMid => (XMin + XMax) / 2;
+    public double YMid => (YMin + YMax) / 2;
+
+    public bool Contains(double x, double y)
+    {
+        return XMin <= x && x < XMax 
+            && YMin <= y && y < YMax;
+    }
+}
+
+internal class MyQuad<T> where T : IPoint
+{
+    public readonly MyQuadTree<T> Source;
+    public readonly Boundary Bounds;
+
+    private readonly List<T> _points = new();
+    private MyQuad<T>[] _children = Array.Empty<MyQuad<T>>();
+
+    public MyQuad(MyQuadTree<T> source, Boundary bounds)
+    {
+        Source = source;
+        Bounds = bounds;
+    }
+
+    public bool Contains(T point)
+    {
+        if (!Bounds.Contains(point.X, point.Y)) return false;
+        if (_children.Length == 0) return _points.Contains(point);
+        return _children.Any(child => child.Contains(point));
+    }
+
+    public void Add(T point)
+    {
+        if (_children.Length == 0)
+        {
+            _points.Add(point);
+            if (_points.Count > Source.QuadCapacity)
+            {
+                Quadify();
+            }
+            return;
+        }
+        foreach (var child in _children)
+        {
+            if (!child.Bounds.Contains(point.X, point.Y)) continue;
+            child.Add(point);
+            break;
+        }
+    }
+
+    private void Quadify()
+    {
+        _children = new MyQuad<T>[4];
+        _children[0] = CreateChild(Bounds.XMin, Bounds.XMid, Bounds.YMid, Bounds.YMax); //top left
+        _children[1] = CreateChild(Bounds.XMid, Bounds.XMax, Bounds.YMid, Bounds.YMax); //top right
+        _children[2] = CreateChild(Bounds.XMin, Bounds.XMid, Bounds.YMin, Bounds.YMid); //bottom left
+        _children[3] = CreateChild(Bounds.XMid, Bounds.XMax, Bounds.YMin, Bounds.YMid); //bottom right
+
+        foreach (var point in _points)
+        {
+            foreach (var child in _children)
+            {
+                if (!child.Bounds.Contains(point.X, point.Y)) continue;
+                child.Add(point);
+                break;
+            }
+        }
+        _points.Clear();
+        _points.Capacity = 0;
+    }
+
+    private MyQuad<T> CreateChild(double xMin, double xMax, double yMin, double yMax)
+    {
+        var bounds = new Boundary(xMin, xMax, yMin, yMax);
+        return new MyQuad<T>(Source, bounds);
+    }
+
+}
